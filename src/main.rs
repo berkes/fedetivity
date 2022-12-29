6cf2a198ec7123e9e24cfdd4c7daad1907cc8f0e
Bèr Kessels <ber@berk.es>
2022-12-29 13:23:13 +0100

    Refactor main to use an isolated WsClient instead of sequential code

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 use std::env;
-
-use awc::Client;
-use futures_util::stream::StreamExt;
-
 use log::{debug, info};
+
+use awc;
+use actix::{Context, Actor, Handler};
+use futures_util::stream::StreamExt;
 
 extern crate fedetivity;
 
 use fedetivity::transmitter::*;
 use fedetivity::determinator::*;
-
-use actix::{Context, Actor, Handler};
 use fedetivity::messages::Job;
 
 struct LogWorker;
@@ -28,21 +26,38 @@ impl Handler<Job> for LogWorker {
     }
 }
 
+struct WsClient {
+    uri: String,
+    client: awc::Client,
+}
+
+impl WsClient {
+    fn new(uri: String) -> Self {
+        let client = awc::Client::builder()
+            .max_http_version(awc::http::Version::HTTP_11)
+            .finish();
+        Self { uri, client }
+    }
+
+    async fn ws_connect(&self) -> Result<(WsFramedSink, WsFramedStream), Box<dyn std::error::Error>> {
+        let (_, framed) = self.client.ws(&self.uri).connect().await?;
+        let (sink, stream): (WsFramedSink, WsFramedStream) = framed.split();
+        Ok((sink, stream))
+    }
+}
 
 #[actix_rt::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
     let ws_uri = env::var("MASTODON_WS_URL").unwrap_or("ws://localhost:3000/ws".to_string());
 
+    debug!("Starting Determinator");
     let determinator = Determinator::start(LogWorker.start().recipient());
 
     debug!("Connecting to {}", ws_uri);
-    let client = Client::builder()
-        .max_http_version(awc::http::Version::HTTP_11)
-        .finish();
-    let (_, framed) = client.ws(ws_uri).connect().await?;
+    let (sink, stream) = WsClient::new(ws_uri).ws_connect().await?;
 
-    let (sink, stream): (WsFramedSink, WsFramedStream) = framed.split();
+    debug!("Starting FedClient");
     let _addr = FedClient::start(determinator.clone().recipient(), sink, stream);
 
     actix_rt::signal::ctrl_c().await?;
